6500e2363377c3105ead890094aeb83cae38b39f
AdrianSzefler <142333547+AdrianSzefler@users.noreply.github.com>
2023-10-02 20:54:10 +0200

    dodano nowe cwiczenie kartka_urodzinowa.pv

---
+++ kartka_urodzinowa.py
@@ -0,0 +1,9 @@
+imie = input("imie:")
+rok_urodzenia = int(input(f"rok urodzenia:"))
+wiadomosc = "Kilograma pomarańczy niech Ci naga baba tańczy"
+imie_nadawcy = "Adrian"
+rok= 2023
+wiek = rok - rok_urodzenia
+print(f"{imie} wszystkiego najlepszego z okazji {wiek} urodzin")
+print(f"{wiadomosc}")
+print(f"{imie_nadawcy}")
